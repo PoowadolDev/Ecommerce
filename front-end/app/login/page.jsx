db62a0b0bdd7912a0fa02b53b9d555b017d56adb
Poowadol Rungtabnapa <Poowadol.dev@gmail.com>
2024-03-29 22:35:21 +0700

    Work on Login Page

---
+++ front-end/app/login/page.jsx
@@ -0,0 +1,20 @@
+export default function Login() {
+    return (
+        <div className="relative h-dvh w-full p-24">
+            <div className="border-2 border-slate-200 h-full w-full rounded-3xl bg-white px-10 py-16">
+                <div className="grid grid-flow-col-dense grid-cols-2 h-full w-full">
+                    <div className=" bg-black">
+                        <div className="grid grid-rows">
+                            <div className="text-black font-sans subpixel-antialiased font-bold text-6xl text-center my-5">
+                                Login
+                            </div>
+                        </div>
+                    </div>
+                    <div className=" bg-slate-600">
+
+                    </div>
+                </div>
+            </div>
+        </div>       
+    )
+}
